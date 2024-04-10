86cbdc170f08a6d4699ce901953a87d6cf32f039
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 16:35:43 +0200

    attempt 4

--- src/script.js
+++ src/script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 function addCar(newCar) {
-    fetch('http://localhost:3001/cars', {
+    fetch('api/cars', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
