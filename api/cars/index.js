fa15542281217c182b562766d7e8842c33cb756a
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 19:05:13 +0200

    Attempt 5

--- api/cars/index.js
+++ api/cars/index.js
@@ -3,6 +3,6 @@ const cars = require('./cars.json');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
-    context.res = cars;
+    context.res.json({some:"somehting"});
     
 }
