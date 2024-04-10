2d29ec69795804d894d9ff08ce9a53e4d09d3a9a
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 18:05:34 +0200

    Attempt 5

--- src/script.js
+++ src/script.js
@@ -3,13 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadCarsBtn = document.getElementById('loadCarsBtn');
     const carList = document.getElementById('carList');
     cars = [];
+    // loadCarsBtn.addEventListener('click', () => {
+    //     fetch('api/cars')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log("received");
+    //             console.log(data);
+    //             cars = data;
+    //             carList.innerHTML = '';
+    //             data.forEach((car, index) => {
+    //                 const carCard = document.createElement('div');
+    //                 carCard.classList.add('car-card');
+    //                 carCard.innerHTML = `
+    //                     <h2>${car.make} ${car.model}</h2>
+    //                     <p><strong>Year:</strong> ${car.year}</p>
+    //                     <p><strong>Make:</strong> ${car.make}</p>
+    //                     <p><strong>Model:</strong> ${car.model}</p>
+    //                     <p><strong>Price:</strong> R${car.price}</p>
+    //                     <button class="btn btn-remove" data-index="${index}">Remove</button>
+    //                 `;
+    //                 carList.appendChild(carCard);
+    //             });
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching car data:', error);
+    //         });
+    // });
     loadCarsBtn.addEventListener('click', () => {
-        fetch('api/cars')
-            .then(response => response.json())
-            .then(data => {
-                console.log("received");
-                console.log(data);
-                cars = data;
+        (async function() {
+            const { data } = await( await fetch(`/api/cars`)).json();
+            cars = data;
                 carList.innerHTML = '';
                 data.forEach((car, index) => {
                     const carCard = document.createElement('div');
@@ -24,10 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     `;
                     carList.appendChild(carCard);
                 });
-            })
-            .catch(error => {
-                console.error('Error fetching car data:', error);
-            });
+        }());
     });
 });
 function addCar(newCar) {
