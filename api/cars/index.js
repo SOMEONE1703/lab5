c6da1ecb06af6a58de56080de29794933f674a66
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 16:18:03 +0200

    attempt 1

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,13 +1,16 @@
+let cars = [];
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    if (req.method=== 'GET'){
+        context.res = data;
+    }
+    if (req.method==='POST'){
+        cars.push(req.body);
+        context.res=req.body;
+    }
+    
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body:"invalid method"
     };
 }
