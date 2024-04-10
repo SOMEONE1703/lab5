2b443728cfb8e2e800346ea6c4289154876f8c13
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 18:19:13 +0200

    Attempt 5

--- api/cars/index.js
+++ api/cars/index.js
@@ -2,16 +2,7 @@ const cars = require('./cars.json');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    if (req.method=== 'GET'){
-        context.res = cars;
-    }
-    if (req.method==='POST'){
-        cars.push(req.body);
-        context.res=req.body;
-    }
-    else{
-        context.res = {
-            body:"invalid method"
-        };
-    }
+    
+    context.res = cars;
+    
 }
